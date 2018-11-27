ce1d5ab149fa7655ef7ec0d28b6e47a6465f5ffc
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2018-11-27 20:30:18 +0100

    Search town by istat code

--- classes/rsshandlers/ProximityRSSHandler.php
+++ classes/rsshandlers/ProximityRSSHandler.php
@@ -5,6 +5,7 @@ class ProximityRSSHandler extends OCRSSHandlerBase
 {
 
   protected $townCode = false;
+  protected $townName = '';
   protected $townLat  = false;
   protected $townLon  = false;
 
@@ -12,16 +13,12 @@ class ProximityRSSHandler extends OCRSSHandlerBase
   function __construct( $townCode )
   {
     $this->townCode = $townCode;
+
+    $this->fetchTown();
   }
 
   function getNodes()
   {
-    if ( empty( $this->townCode ) )
-    {
-      throw new Exception( "Code not present" );
-    }
-
-
     //(raw[!geofilt pt=11.12576,46.066423 sfield=subattr_geo___coordinates____gpt d=5]) and (raw[subattr_geo___longitude____f] = '*' and raw[subattr_geo___latitude____f] = '*')
     //41.9101776,12.4659587
     $searchParameters = array(
@@ -47,16 +44,59 @@ class ProximityRSSHandler extends OCRSSHandlerBase
 
     // Verificare che sia
     if ( $this->townLat && $this->townLon ) {
-      $filters []= '(attr_condivisione_comuni_b:true AND fq:{!geofilt pt=12.4659587,41.9101776 sfield=subattr_geo___coordinates____gpt d=5}';
+      $filters []= '(attr_condivisione_comuni_b:true AND fq:{!geofilt pt='.$this->townLon.','.$this->townLat.' sfield=subattr_geo___coordinates____gpt d=5})';
     }
 
     return array(implode(' OR ', $filters));
   }
 
+  protected function fetchTown()
+  {
+    if (  $this->townCode )
+    {
+      $searchParameters = array(
+        'limit' => '1',
+        'class_id' => array('comune'),
+        'filter' => array('attr_istat_s:' . $this->townCode),
+      );
+
+      $searchResult = eZFunctionHandler::execute( 'ezfind', 'search', $searchParameters );
+      if ($searchResult['SearchCount'] > 0) {
+
+        /** @var eZContentObject $object */
+        $object = $searchResult['SearchResult'][0]->ContentObject;
+
+        $this->townName = $object->Name;
+
+        $dataMap = $object->dataMap();
+
+        /*
+
+        1|#46.066423|#11.12576|#Trento, Territorio Val d'Adige, TN, Trentino-Alto Adige, Italia
+        eZGmapLocation Object
+        (
+            [PersistentDataDirty] =>
+            [contentobject_attribute_id] => 168188
+            [contentobject_version] => 2
+            [latitude] => 46.066423
+            [longitude] => 11.12576
+            [street] => Trento, Territorio Val d'Adige, TN, Trentino-Alto Adige, Italia
+        )
+
+         */
+
+        if ($dataMap['geo']->hasContent()) {
+          $this->townLat = $dataMap['geo']->content()->latitude;
+          $this->townLon = $dataMap['geo']->content()->longitude;
+        }
+      }
+    }
+  }
+
 
   function getFeedTitle()
   {
-    // TODO: Implement getFeedTitle() method.
+    return 'News per il comune di ' . $this->townName;
   }
 
   function getFeedAccessUrl()
